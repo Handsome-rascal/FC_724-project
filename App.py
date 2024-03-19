4a5ffab3790e87ba7fcf96897c8099b742592f41
Handsome-rascal <148557035+Handsome-rascal@users.noreply.github.com>
2024-03-19 16:15:08 +0000

    added feedback.txt file and thank you html page. Also added data collection route to App.py which allows for submision of data to feedback.txt
    fixed error in data required where i forgot to put () at the end of it, also added data collection route that will take data added to form and put it in feedback.txt file which was created and added to the project. also made thank you html page and route for it which it will take you to when form is successfuly submitted.

--- App.py
+++ App.py
@@ -10,12 +10,12 @@ app.config["SECRET_KEY"] = "password"
 # form to collect data
 class DataCollectionForm(flask_wtf.FlaskForm):
     # form fields with validation requirements
-    name = wtforms.StringField("Name", validators=[wtforms.validators.DataRequired])
-    student_number = wtforms.StringField("Student number", validators=[wtforms.validators.DataRequired])
-    email = wtforms.EmailField("Email address", validators=[wtforms.validators.DataRequired])
-    grades = wtforms.TextAreaField("Grades obtained in course", validators=[wtforms.validators.DataRequired])
-    Satisfaction = wtforms.RadioField("Overall satisfaction with the course",choices=[("very-satisfied","Very satisfied"),("satisfied","Satisfied"),("nuetral","Neutral"),("dissatisfied","Dissatisfied"),("very-dissatisfied","Very dissatisfied")])
-    improvement = wtforms.TextAreaField("Suggestions for improvement", validators=[wtforms.validators.DataRequired])
+    name = wtforms.StringField("Name", validators=[wtforms.validators.DataRequired()])
+    student_number = wtforms.StringField("Student number", validators=[wtforms.validators.DataRequired()])
+    email = wtforms.EmailField("Email address", validators=[wtforms.validators.DataRequired()])
+    grades = wtforms.TextAreaField("Grades obtained in course", validators=[wtforms.validators.DataRequired()])
+    satisfaction = wtforms.RadioField("Overall satisfaction with the course",choices=[("very-satisfied","Very satisfied"),("satisfied","Satisfied"),("nuetral","Neutral"),("dissatisfied","Dissatisfied"),("very-dissatisfied","Very dissatisfied")])
+    improvement = wtforms.TextAreaField("Suggestions for improvement", validators=[wtforms.validators.DataRequired()])
     submit = wtforms.SubmitField("Submit")
 
 
@@ -28,3 +28,18 @@ def welcome_page():
 @app.route("/information_page")
 def information_page():
     return flask.render_template("information_page.html")
+
+@app.route("/data_collection", methods=["GET","POST"])
+def data_collection():
+    form = DataCollectionForm()
+    if form.validate_on_submit():
+        with open('feedback.txt', 'a') as file:
+            file.write(
+                f'Name: {form.name.data}, Student number: {form.student_number.data}, Email: {form.email.data}, Grades: {form.grades.data}, Satisfaction: {form.satisfaction.data}, Improvement: {form.improvement.data}\n')
+        return flask.redirect(flask.url_for('thank_you'))
+    return flask.render_template('data_collection.html', form=form)
+
+@app.route('/thank_you')
+def thank_you():
+    return flask.render_template('thank_you.html')
+app.run()
